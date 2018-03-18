e16e80f334e6211066f57a4b9e3007d8e865a219
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2018-03-18 14:31:10 +0100

    Bugfix: actually zero the memory in the compiled program. This might be security-relevant, because without this fix, the Brainfuck program might read uninitialized memory.

--- brainfuck/bf.py
+++ brainfuck/bf.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+#    bf.py
+#    Copyright (C) 2017-2018 by CJP
+#
+#    This file is part of Brainfuck-compiler-interpreter.
+#
+#    Brainfuck-compiler-interpreter is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Brainfuck-compiler-interpreter is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Brainfuck-compiler-interpreter. If not, see <http://www.gnu.org/licenses/>.
+
 from collections import defaultdict
 import sys
 from collections import deque
@@ -229,6 +248,7 @@ def compileCode():
 '''
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXMEM %d
 unsigned char memory[MAXMEM];
@@ -244,6 +264,8 @@ void incrementInstrCount()
 
 int main(int argc, char **argv)
 {
+	memset(memory, 0, MAXMEM);
+
 ''' % (maxMemory, maxInstructions)
 
 	for p in program:
